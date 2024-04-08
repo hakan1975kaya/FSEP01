2d70087c0e346d183538a164be644175461032b4
HAKAN KAYA <hakankayaed@gmail.com>
2024-04-08 17:41:58 +0300

    Machine Tables created

---
+++ Entities/Concrete/Entities/General/InputCoilBasicData.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete.Entities.General
+{
+    public class InputCoilBasicData : IEntity
+    {
+        public Guid Id { get; set; }
+        public Guid InputCoilId { get; set; }
+        public short? Acceleration { get; set; }
+        public short? Deceleration { get; set; }
+        public short? FastStop { get; set; }
+        public short? JogSpeed { get; set; }
+        public DateTime Optime { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
+
---
+++ Entities/Concrete/Entities/General/ParameterApplyingUnit.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete.Entities.General
+{
+    public class ParameterApplyingUnit:IEntity
+    {
+        public Guid Id { get; set; }
+        public Guid ParameterId { get; set; }
+        public bool? LubricatorAutoOn { get; set; }
+        public decimal? RelativeApplyingSpeedSet { get; set; }
+        public decimal? MinimumApplyingSpeedSet { get; set; }
+        public decimal? ApplyingUnitTempTrayOneSet { get; set; }
+        public decimal? ApplyingUnitTempTrayTwoSet { get; set; }
+        public short? PressureAROneLeftSideSet { get; set; }
+        public short? PressureAROneRightSideSet { get; set; }
+        public DateTime Optime { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
