e6badb415a5a65197fd29179fdddebfb013a68f4
HAKAN KAYA <hakankayaed@gmail.com>
2024-05-02 16:58:52 +0300

    Recipe deveoped

---
+++ Business/Concrete/PLC/Transfers/PLCManager.cs
@@ -0,0 +1,22 @@
+﻿
+using Business.Abstract.PLC.Transfers;
+using Business.Constants.Messages.PLC.Recipe;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract.PLC;
+using Entities.Concrete.Entities.PLC.Recipe;
+using PLC.Abstract;
+using S7.Net;
+
+namespace Business.Concrete.PLC
+{
+    public class PLCManager : IPLCService
+    {
+        public PLCManager()
+        {
+
+        }
+
+
+    }
+}
